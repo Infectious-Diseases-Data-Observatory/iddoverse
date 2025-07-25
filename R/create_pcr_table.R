b2bb788c218c7a5468c65c73f8b6e7e6791cb5c1
Rhys Peploe <rhys.peploe@iddo.org>
2025-07-25 18:03:23 +0100

    Init commit

---
+++ R/create_pcr_table.R
@@ -0,0 +1,9 @@
+create_pcr_table = function(pf_domain,
+                            rs_domain){
+  full_join(
+    prepare_domain("PF", pf_domain, variables_include = "INTP"),
+    prepare_domain("rs", rs_domain, variables_include = "WHOMAL01")
+  )
+}
+
+
