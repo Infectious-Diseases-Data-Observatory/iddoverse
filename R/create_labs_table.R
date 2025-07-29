c7c88b4d9366ccd80015221064addbef4bbd0048
Rhys Peploe <rhys.peploe@iddo.org>
2025-07-29 13:30:02 +0100

    Init commit

---
+++ R/create_labs_table.R
@@ -0,0 +1,17 @@
+create_labs_table <- function(lb_domain,
+                              variables = c("ALB", "ALT", "AST", "BILI", "CREAT",
+                                            "CD4", "G6PD", "HCT", "HGB", "HGBMET",
+                                            "INTLK6", "K", "PLAT", "SODIUM", "WBC"),
+                              include_method = FALSE,
+                              timing_variables = c(
+                                "LBHR", "LBDY", "LBSTDY", "VISITDY", "VISITNUM",
+                                "VISIT", "EPOCH", "LBEVLINT", "LBEVINTX"),
+                              values_funct = first){
+
+  prepare_domain("lb", lb_domain,
+                 include_METHOD = include_method,
+                 variables_include = variables,
+                 timing_variables = timing_variables,
+                 values_fn = values_funct)
+}
+
