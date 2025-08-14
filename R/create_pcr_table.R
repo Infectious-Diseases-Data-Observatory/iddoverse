0137fff8c6ba95ed9dc734e8a50f06ecd885731b
Rhys Peploe <rhys.peploe@iddo.org>
2025-08-14 09:35:38 +0100

    Rename

--- R/create_pcr_table.R
+++
@@ -1,8 +0,0 @@
-create_pcr_table <- function(pf_domain, rs_domain){
-  full_join(
-    prepare_domain("PF", pf_domain, variables_include = "INTP"),
-    prepare_domain("rs", rs_domain, variables_include = "WHOMAL01")
-  )
-}
-
-
