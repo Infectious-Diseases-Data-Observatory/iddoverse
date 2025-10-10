3fc5cedbd4505f1628d803ba0d10273a02aecf02
Rhys Peploe <rhys.peploe@iddo.org>
2025-10-10 13:38:37 +0100

    Improved documentation, added example

--- R/create_labs_table.R
+++ R/create_labs_table.R
@@ -3,24 +3,38 @@
 #' Uses the IDDO-SDTM laboratory test results (LB) domain to create a single
 #' dataset with various lab test result information summarised.
 #'
-#' @param lb_domain The name of the laboratory test results (LB) domain in the
-#'   global environment.
-#' @param variables A list of variables to be included in the output dataset.
-#' @param include_method Boolean. Should LBMETHOD be included in the output for all variables.
-#' @param timing_variables List of timing variables which are to be used to
-#'   separate time points, this is hierarchical so the order is taken into
-#'   account. Default is: LBHR, LBDY, LBSTDY, VISITDY, VISITNUM, VISIT, EPOCH,
-#'   LBEVLINT, LBEVINTX.
-#' @param values_funct The function which will determine which data row is used in
-#'   the output, in the event there are multiple rows for the same subject with
-#'   the same time points (as listed in timing_variables). Default is first(),
-#'   i.e. if there is two rows from the same day and time, the first record will
-#'   be taken, the second will be dropped. Choice of timing_variables will
-#'   impact the number of rows affected.
+#' @param lb_domain A laboratory/LB domain data frame.
+#' @param variables Character list. A list of variables to be included in the
+#'   output dataset.
+#' @param include_method Boolean. Should LBMETHOD be included in the output for
+#'   all variables.
+#' @param timing_variables Character list. List of timing variables which are to
+#'   be used to separate time points, this is hierarchical so the order is taken
+#'   into account. Default is: LBHR, LBDY, LBSTDY, VISITDY, VISITNUM, VISIT,
+#'   EPOCH, LBEVLINT, LBEVINTX.
+#'
+#'   (using default for example) Each row will be initially summarised based on
+#'   the --HR (study hour) variable, if that is missing then the --DY (study
+#'   day) variable is used, and so on. The output will be one row per
+#'   participant, per time point, where the time point for each row is the first
+#'   available variable listed in timing_variables.
+#' @param values_funct Function. The function which will determine which data
+#'   row is used in the output, in the event there are multiple rows for the
+#'   same subject with the same time points (as listed in timing_variables).
+#'   Default is first(), i.e. if there is two rows from the same day and time,
+#'   the first record will be taken, the second will be dropped. Choice of
+#'   timing_variables will impact the number of rows affected.
 #'
 #' @returns An analysis dataset, one row per person, per timepoint.
 #'
 #' @export
+#'
+#' @examples
+#' create_labs_table(LB_RPTESTB)
+#'
+#' create_labs_table(LB_RPTESTB,
+#'                   variables = c("HGB"),
+#'                   timing_variables = c("EPOCH", "VISIT"))
 #'
 
 create_labs_table <- function(lb_domain,
