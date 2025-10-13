516d1aff0aec0899bbea19853540fa924fb989e9
Rhys Peploe <rhys.peploe@iddo.org>
2025-10-13 15:34:38 +0100

    Removed timing_variables as the domain choice was impacting the prepare_domain function and in turn the merging of dataset

--- R/create_clinical_table.R
+++ R/create_clinical_table.R
@@ -9,17 +9,6 @@
 #' @param mp_domain A morphology and physiology/MP domain data frame.
 #' @param sa_domain A clinical and adverse events/SA domain data frame.
 #' @param vs_domain A vital signs/VS domain data frame.
-#' @param timing_variables Character list. List of timing variables which are to
-#'   be used to separate time points, this is hierarchical so the order is taken
-#'   into account. Default is: --HR, --DY, --STDY, VISITDY, VISITNUM, VISIT,
-#'   EPOCH,
-#'   --EVLINT, --EVINTX.
-#'
-#'   (using default for example) Each row will be initially summarised based on
-#'   the --HR (study hour) variable, if that is missing then the --DY (study
-#'   day) variable is used, and so on. The output will be one row per
-#'   participant, per time point, where the time point for each row is the first
-#'   available variable listed in timing_variables.
 #' @param values_funct Function. The function which will determine which data row is used
 #'   in the output, in the event there are multiple rows for the same subject
 #'   with the same time points (as listed in timing_variables). Default is
@@ -37,53 +26,37 @@
 #'                       sa_domain = SA_RPTESTB,
 #'                       vs_domain = VS_RPTESTB)
 #'
-#' create_clinical_table(dm_domain = DM_RPTESTB,
-#'                       sa_domain = SA_RPTESTB,
-#'                       vs_domain = VS_RPTESTB)
-#'
 create_clinical_table <- function(dm_domain, mb_domain = NULL, mp_domain = NULL,
                                   sa_domain = NULL, vs_domain = NULL,
-                                  timing_variables = c(
-                                    str_c(domain, "HR"), str_c(domain, "DY"),
-                                    str_c(domain, "STDY"), "VISITDY", "VISITNUM",
-                                    "VISIT", "EPOCH", str_c(domain, "EVLINT"), str_c(domain, "EVINTX")),
                                   values_funct = first){
   data <- prepare_domain("dm", dm_domain, variables_include = c("STUDYID", "USUBJID"))
 
   if(!is.null(mb_domain)){
-    domain <- "MB"
     data <- data %>%
       full_join(prepare_domain("mb", mb_domain, variables_include = c("MTB", "HIV"),
-                               timing_variables = timing_variables,
                                values_fn = values_funct))
   }
 
   if(!is.null(mp_domain)){
-    domain <- "MP"
     data <- data %>%
       full_join(prepare_domain("mp", mp_domain, include_LOC = TRUE,
-                               timing_variables = timing_variables,
                                values_fn = values_funct))
   }
 
 
   if(!is.null(vs_domain)){
-    domain <- "VS"
     data <- data %>%
       full_join(prepare_domain("vs", vs_domain,
-                               timing_variables = timing_variables,
                                values_fn = values_funct))
   }
 
   if(!is.null(sa_domain)){
-    domain <- "SA"
     data <- data %>%
       full_join(prepare_domain("sa", sa_domain,
                                variables_include = c(
                                  "vomiting", "nausea", "fever",  "diarrhea",
                                  "abdominal pain", "jaundice", "bleeding",
                                  "anemia", "anorexia", "blood transfusion"),
-                               timing_variables = timing_variables,
                                values_fn = values_funct))
   }
 
