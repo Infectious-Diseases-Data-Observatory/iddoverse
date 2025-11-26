36f5534dec3bc5e94bbdd209854f476425d53782
Rhys Peploe <rhys.peploe@iddo.org>
2025-11-26 17:44:48 +0000

    add global variables

--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,38 @@
 `%!in%` <- Negate(`%in%`)
+
+globalVariables(c(
+  "STUDYID",
+  "USUBJID",
+  "TIME",
+  "TIME_SOURCE",
+  "AGEU",
+  "AGE_YEARS",
+  "AGE_DAYS",
+  "TESTCD",
+  "n",
+  "UNITS",
+  "RESULTS",
+  "LOC",
+  "METHOD",
+  "EVENT",
+  "PRESP",
+  "OCCUR",
+  "SACAT",
+
+  #--------- CHECK DATA
+  "STRESN",
+  "STRESC",
+  "STRESU",
+  "SPEC",
+  "VISITDY",
+  "EPOCH",
+  "column",
+  "proportion_missing",
+  #--------- SUM PARA
+  ".",
+  "TPARA",
+  "POS_IND_CHAR",
+  "POS_IND_NUM"
+  #---------
+  ))
+
