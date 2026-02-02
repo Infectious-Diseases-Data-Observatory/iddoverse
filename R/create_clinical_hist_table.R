888a752707b5f1953d42504d4b53e2341e781148
Rhys Peploe <rhyspeploe1998@gmail.com>
2026-02-02 12:18:28 +0000

    Remove dm after function

--- R/create_clinical_hist_table.R
+++ R/create_clinical_hist_table.R
@@ -4,7 +4,6 @@
 #' dataset with various historic clinical information summarised. Historic
 #' refers to events that occurred before the study start.
 #'
-#' @param dm_domain A demographics/DM domain data frame.
 #' @param sa_domain A clinical and adverse events/SA domain data frame.
 #' @param timing_variables Character list. List of timing variables which are to be used to
 #'   separate time points, this is hierarchical so the order is taken into
@@ -28,33 +27,25 @@
 #' @export
 #'
 #' @examples
-#' create_clinical_hist_table(DM_RPTESTB, SA_RPTESTB)
+#' create_clinical_hist_table(SA_RPTESTB)
 #'
 #' # Change which timing_variables are used to summarise the data
 #' create_clinical_hist_table(DM_RPTESTB, SA_RPTESTB,
 #'                            timing_variables = c("VISITDY", "EPOCH"))
 #'
-create_clinical_hist_table <- function(dm_domain, sa_domain,
+create_clinical_hist_table <- function(sa_domain,
                                        timing_variables = c(
                                          "SAHR", "SADY", "SASTDY", "VISITDY", "VISITNUM",
                                          "VISIT", "EPOCH", "SAEVLINT", "SAEVINTX"),
                                        values_funct = first){
   assert_data_frame(sa_domain, required_vars = exprs(SACAT))
 
-  data_dm <- prepare_domain("dm", dm_domain, variables_include = c("STUDYID", "USUBJID"))
-
   data_sa <- sa_domain %>%
-      filter(SACAT == "MEDICAL HISTORY")
+         filter(SACAT == "MEDICAL HISTORY")
 
-  data <- data_dm %>%
-    right_join(prepare_domain("SA",
-                              data_sa,
-                             variables_include = c(
-                               "fever", "anemia",
-                               "malaria", "hiv"),
-                             timing_variables = timing_variables, values_fn = values_funct))
-
-  data <- data %>%
+  data <- prepare_domain("SA", data_sa,
+                 variables_include = c("fever", "anemia", "malaria", "hiv"),
+                 timing_variables = timing_variables, values_fn = values_funct) %>%
     remove_empty("cols")
 
   return(data)
