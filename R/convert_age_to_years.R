ccb2bba7eb47cb894a7f33a1e4dd981087cee6ff
Rhys Peploe <rhyspeploe1998@gmail.com>
2026-08-05 12:05:10 +0100

    Add NA as an option for AGEU

--- R/convert_age_to_years.R
+++ R/convert_age_to_years.R
@@ -24,8 +24,8 @@ convert_age_to_years <- function(data) {
   if(nrow(data) == 0){
     # skip to end if empty
   } else{
-    if(any(data$AGEU %in% c("HOURS", "DAYS", "WEEKS", "MONTHS", "YEARS") == FALSE)){
-      rlang::abort("There exists a non-standard AGEU (age units) which is not HOURS, DAYS, WEEKS, MONTHS or YEARS. Convert this manually before using convert_age_to_years")
+    if(any(data$AGEU %in% c("HOURS", "DAYS", "WEEKS", "MONTHS", "YEARS", NA, NA_character_) == FALSE)){
+      rlang::abort("There exists a non-standard AGEU (age units) which is not HOURS, DAYS, WEEKS, MONTHS, YEARS or NA. Convert this manually before using convert_age_to_years")
     }
 
     for (i in seq(1, nrow(data), 1)) {
