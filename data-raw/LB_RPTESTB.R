476328e39dfe468e14475f199e731985c819e777
Rhys Peploe <rhyspeploe1998@gmail.com>
2026-09-18 15:50:02 +0100

    add package::

--- data-raw/LB_RPTESTB.R
+++ data-raw/LB_RPTESTB.R
@@ -1,7 +1,7 @@
 ## code to prepare `LB_RPTESTB` dataset goes here
 
-LB_RPTESTB <- read_csv("inst/extdata/Synthetic_Data_RPTESTB/LB_RPTESTB.csv",
-                       show_col_types = FALSE, guess_max = Inf)
+LB_RPTESTB <- readr::read_csv("inst/extdata/Synthetic_Data_RPTESTB/LB_RPTESTB.csv",
+                              show_col_types = FALSE, guess_max = Inf)
 
 usethis::use_data(LB_RPTESTB, overwrite = TRUE)
 
